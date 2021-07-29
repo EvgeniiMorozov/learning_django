38a4bf894f61f8254482b1cd422447b2f0f7af59
EvgeniiMorozov <emorozov1101@gmail.com>
2021-07-29 20:16:26 +0500

    modify

--- my_test_site/news/models.py
+++ my_test_site/news/models.py
@@ -9,9 +9,9 @@ class News(models.Model):
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото", blank=True)
     is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
-    
+
     class Meta:
         verbose_name = "Новость"
         verbose_name_plural = "Новости"
