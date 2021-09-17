b76eda38ab3516c5462ad9122e2d245c5f47ac03
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-17 13:32:46 +0500

    урок 24

--- my_test_site/news/forms.py
+++ my_test_site/news/forms.py
@@ -4,7 +4,30 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(
+        max_length=150,
+        label='Название статьи',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+        })
+    )
+    content = forms.CharField(
+        label='Текст статьи',
+        required=False,
+        widget=forms.Textarea({
+            'class': 'form-control',
+            'rows': '3',
+        })
+    )
+    is_published = forms.BooleanField(
+        label='опубликованно?',
+        initial=True,
+    )
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        label='Категория',
+        empty_label='Выберите категорию',
+        widget=forms.Select(attrs={
+            'class': 'form-control',
+        })
+    )
